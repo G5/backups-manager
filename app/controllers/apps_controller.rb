d2d09a8c38904ff7118bc9dd693653ffbf54c9c3
johnlucia <johnmlucia@gmail.com>
2015-05-11 22:14:10 -0700

    basic idea of an index working, now for some refactor

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -4,10 +4,23 @@ class AppsController < ApplicationController
   require "uri"
 
   def index
-    response = RestClient.get "https://api.heroku.com/apps", :authorization => "Bearer #{ENV['HEROKU_AUTH_TOKEN']}", :accept => "application/vnd.heroku+json; version=3"
+    uri = "https://api.heroku.com/apps"
+    headers = { authorization: "Bearer #{ENV['HEROKU_AUTH_TOKEN']}",
+                accept: "application/vnd.heroku+json; version=3",
+                range: "name ..; max=1000;"
+              }
+    response = RestClient.get uri, headers
 
-    @data = JSON.parse(response.body)
+    data = JSON.parse response.body
 
+    while response.code == 206
+      headers[:range] = response.headers[:next_range]
+      response = RestClient.get uri, headers
+      next_batch = JSON.parse response.body
+      data += next_batch
+    end
+
+    @data = data.group_by { |app| app["owner"]["email"] }
   end
 
   def show
