68170a1e7061d1143e2bba0b98befef85ae4c11a
Steve Craig <steve.craig@getg5.com>
2015-06-15 15:48:13 -0700

    add cls to version list

--- lib/app_list.rb
+++ lib/app_list.rb
@@ -59,6 +59,7 @@ class AppList
 
   def self.version_apps_list
     {
+      'cls' => "https://raw.githubusercontent.com/g5search/g5-client-leads-service/master/config/version.yml?token=AFfFnC-HYV_FvflbpbjsWdBlqJcEdZKfks5ViJCIwA%3D%3D",
       'cms' => "https://raw.githubusercontent.com/g5search/g5-content-management-system/master/config/version.yml",
       'dsh' => "https://raw.githubusercontent.com/g5search/g5-dashboard/master/config/version.yml?token=AFfFnAGZ3yANo_-lbi2Bb8h8kfedepSYks5Vf0XGwA%3D%3D"
     }
