97dc42887b1fd7352c0512bf8178322a9ba15b93
Ryan Bales <ryankbales@gmail.com>
2015-07-06 12:05:56 -0700

    adding workers to schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,25 @@
 # Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
 # Learn more: http://github.com/javan/whenever
 
-  # every 1.day, :at => '7:00 am' do
-  #   runner "AppWranglerWorker.perform_async"
-  # end
+
+# *********UTC Time Zone For Heroku Servers************
+every 1.day, :at => '7:00 am' do
+  runner "AppWranglerWorker.perform_async"
+end
+
+every 1.day, :at => '8:00 am' do
+  runner "AppAddonsWorker.perform_async"
+end
+
+every 1.day, :at => '9:00 am' do
+  runner "AppConfigVarsWorker.perform_async"
+end
+
+every 1.day, :at => '10:00 am' do
+  runner "AppDomainsWorker.perform_async"
+end
+
+every 1.day, :at => '11:00 am' do
+  runner "AppDynosWorker.perform_async"
+end
