3374f4f50ef26bc25cf00b89598cf6589d45bfeb
Ryan Bales <ryankbales@gmail.com>
2015-07-06 09:56:16 -0700

    refactoring logic in view

--- app/workers/app_addons_worker.rb
+++ app/workers/app_addons_worker.rb
@@ -3,6 +3,7 @@ class AppAddonsWorker
   include Sidekiq::Worker
 
   def perform
+    app_list = AppList.new().get_app_list
     App.all.each do |app|
       addons = AppDetails.new(app.name).get_app_addons
       app.update_attributes({addons: addons})
