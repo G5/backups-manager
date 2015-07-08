ae671dccdb083dfb116b6906f20e3f1c4558b8e7
Ryan Bales <ryankbales@gmail.com>
2015-07-08 08:46:57 -0700

    globalizing versioning methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  @@masters = nil
+
   def exclude_config_var(var_name)
     excluded_vars = [ "SECRET",
                       "ID_RSA",
@@ -24,45 +24,10 @@ module ApplicationHelper
     "<i>#{value.size} apps</i>"
   end
 
-  def master_versions
-    @@masters ||= version_apps_list.inject({}) do |h, (k, v)|
-      h[k] = get_app_master_version(k)
-      h
-    end
-  end
-
-  def get_master_version(key)
-    master_versions.try(:[], key.downcase.parameterize)
-  end
-
-  def get_app_master_version(type)
-    uri = URI(version_apps_list[get_app_version_type(type)]) if version_apps_list.has_key?(get_app_version_type(type))
-    content = Net::HTTP.get(uri) if uri
-    yml = YAML.load(content).try(:with_indifferent_access) if content && content != 'Not Found'
-    yml.try(:[], :version) if yml
-  end
-
-  def get_app_version_type(app)
-    version_list = version_apps_list
-    arr = version_list.map do |k, v|
-      k if app == k
-    end.compact
-    arr.empty? ? nil : arr.first
-  end
-
   def regular_app_groups
     [ 'g5-analytics', 'g5-backups', 'g5-cau', 'g5-client', 'g5-cls', 'g5-clw', 'g5-cms-',
       'g5-cpas', 'g5-cpns', 'g5-cxm', 'g5-dsh', 'g5-inventory', 'g5-inv-', 'g5-jobs', 'g5-layout',
-      'g5-nae', 'g5-social', 'g5-theme-', 'g5-vendor', 'g5-vls', 'g5-widget']
-  end
-
-  def version_apps_list
-    {
-      'cau' => "https://raw.githubusercontent.com/G5/g5-sibling-deployer/master/config/version.yml",
-      'cls' => "https://raw.githubusercontent.com/g5search/g5-client-leads-service/master/config/version.yml?token=#{ENV['CLS_GITHUB_TOKEN']}",
-      'cms' => "https://raw.githubusercontent.com/g5search/g5-content-management-system/master/config/version.yml",
-      'dsh' => "https://raw.githubusercontent.com/g5search/g5-dashboard/master/config/version.yml?token=#{ENV['DSH_GITHUB_TOKEN']}"
-    }
+      'g5-nae', 'g5-social', 'g5-theme-', 'g5-vendor', 'g5-vls', 'g5-widget', 'g5-app-wrangler']
   end
 
   def dyno_count(dynos)
@@ -84,5 +49,13 @@ module ApplicationHelper
     end
     average = grand_total.to_f/apps.count
     average.round(1)
+  end
+
+  def versioned_apps
+    AppVersions.new().version_apps_list
+  end
+
+  def get_mv(type)
+    AppVersions.new().get_master_version(type)
   end
 end
--- app/helpers/orgs_helper.rb
+++ app/helpers/orgs_helper.rb
@@ -1,16 +1,9 @@
 module OrgsHelper
-  def master_version_inputs
-    version_apps_list.inject("") do |str, (key, value)|
-      master_version = get_master_version(key)
-      str << "<input type='hidden' id='master-version-#{key}' value='#{master_version}'>"
-      str
-    end.html_safe
-  end
 
   def get_app_type(app)
     type_array = app.name.split("-")
     type = ""
-    version_apps_list.each do |key, value|
+    versioned_apps.each do |key, value|
       type = key if type_array.include?(key)
     end
     type
