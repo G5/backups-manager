3acaa84da526c1d9f39f50aa17d0f8eaa399f3e5
Ryan Bales <ryankbales@gmail.com>
2015-07-13 08:48:05 -0700

    deleting whenever stuff, wip on removing apps

--- app/workers/remove_deleted_app_worker.rb
+++ app/workers/remove_deleted_app_worker.rb
@@ -1,12 +1,16 @@
-# class RemoveDeletedAppWorker
-#   def perform
-#     remove_deleted_app
-#   end
+class RemoveDeletedAppWorker
+  def perform
+    remove_deleted_app
+  end
 
-#   def remove_deleted_app
-#     app_list = AppList.new().get_app_list #hits api once
-#     apps_in_database = App.all
+  def remove_deleted_app
+    app_list = AppList.new().get_app_list #hits api once
+    apps_in_database = App.all
 
-    
-#   end
-# end
+    apps_in_database.each do |app|
+      app_list.each do |happ|
+        happ["name"]
+      end
+    end 
+  end
+end
