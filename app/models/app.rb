4909a4c1afe43c59ed470bbfb244fab4049ad00a
John Mendonca <joaosinho@gmail.com>
2015-07-15 15:38:02 -0700

    Move get_app_type to App.type_from_name

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,13 @@
 class App < ActiveRecord::Base
-  
+  def type
+    App.type_from_name(self.name)
+  end
+
+  def self.type_from_name(name)
+    return "" unless name.present?
+    name_tokens = name.split("-")
+    common = AppVersions.app_list.keys & name_tokens
+    return "" unless common.present? && common.length == 1
+    common.first
+  end
 end
