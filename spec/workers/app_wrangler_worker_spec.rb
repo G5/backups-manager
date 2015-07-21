6e6a7a5b6883bf9dbb2748781f217dff53ec8aa7
John Mendonca <joaosinho@gmail.com>
2015-07-21 12:45:21 -0700

    AppWranglerWorker spec

---
+++ spec/workers/app_wrangler_worker_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe AppWranglerWorker do
+  describe :perform do
+    let(:app_list) { [{"name" => "App"}] }
+    before { allow(AppList).to receive(:get).and_return(app_list)}
+
+    context "when an app doesn't exist" do
+      it "creates a new app with the correct data" do
+        expect(App.count).to eq(0)
+        AppWranglerWorker.perform_async
+        expect(App.count).to eq(1)
+        expect(App.first.app_details["name"]).to eq(app_list[0]["name"])
+      end
+    end
+
+    context "when an app already exists in the database" do
+      it "updates the existing app without creating a new one" do
+        App.create(id: 1, name: "App")
+        expect(App.count).to eq(1)
+        AppWranglerWorker.perform_async
+        expect(App.count).to eq(1)
+      end
+    end
+  end
+end
