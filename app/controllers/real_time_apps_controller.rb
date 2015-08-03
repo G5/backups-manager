647bcfd0765d88289b0467caecdf259e5b299a65
johnlucia <johnmlucia@gmail.com>
2015-08-03 09:28:54 -0700

    put the json view back

--- app/controllers/real_time_apps_controller.rb
+++ app/controllers/real_time_apps_controller.rb
@@ -2,16 +2,26 @@ class RealTimeAppsController < ApplicationController
   def index
     @app_list = AppList.sorted
 
-    # Need to clean this up. Duplicated over in the OrgsController
-    zone = ActiveSupport::TimeZone.new("Pacific Time (US & Canada)")
-    @time = Time.now.in_time_zone(zone).strftime("%I:%M%p")
-
     respond_to do |format|
       format.html
-      format.json { render json: AppList.new.make_app_array.as_json }
+      format.json { render json: stripped_app_list.as_json }
     end
   end
 
   def show
   end
+
+  private
+
+  def stripped_app_list
+    stripped_list = {}
+    @app_list.each do |app_type, app_details|
+      stripped_list[app_type.to_sym] = []
+      @app_list[app_type.to_sym].each do |app|
+        stripped_list[app_type] << app["name"]
+      end
+    end
+    stripped_list
+  end
+
 end
