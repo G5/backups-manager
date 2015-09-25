a8ca8b15139db423c86c0127c7f8ba749600eb01
Ryan Bales <ryankbales@gmail.com>
2015-09-25 14:47:36 -0700

    adds txt format to app type list

--- app/controllers/app_types_controller.rb
+++ app/controllers/app_types_controller.rb
@@ -8,5 +8,9 @@ class AppTypesController < ApplicationController
     @type = params[:id]
     @apps = App.group_by_type(App.all)[@type]
     raise ActiveRecord::RecordNotFound.new("can't find apps of type '#{@type}'!") if @apps.blank?
+    respond_to do |format|
+      format.html
+      format.txt
+    end
   end
 end
