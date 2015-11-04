3229f6607129a5640fc26133a13e1415d8505fab
johnlucia <johnmlucia@gmail.com>
2015-11-04 15:09:19 -0800

    some notes on that stuff

--- lib/cms_tar_ball.rb
+++ lib/cms_tar_ball.rb
@@ -17,6 +17,8 @@ class CmsTarBall
 
   private
 
+  # Set up public GET and PUT urls on S3 to store the tar.gz we downloaded from Github
+  # Note: Both these URL's expire in one hour.
   def generate_sources_endpoints
     response = HTTPClient.post @heroku_app_sources_endpoint, nil, HerokuApiHelpers.default_headers
     data = JSON.parse response.body
@@ -24,21 +26,21 @@ class CmsTarBall
     @heroku_put_url = data["source_blob"]["put_url"]
   end
 
+  # Download tar.gz from Github
   def download
     system("curl -H 'Authorization: token #{ENV['GITHUB_TOKEN']}' -L #{@github_file_location} > #{@local_file_location}")
   end
 
+  # Upload tar.gz to S3
   def upload
     system("curl \"#{@heroku_put_url}\" -X PUT -H 'Content-Type:' --data-binary @#{@local_file_location}")
   end
 
+  # Tell Heroku to deploy the CMS from the tar.gz stored at @heroku_get_url
   def deploy_cms
     system "curl -n -X POST \"#{@heroku_build_endpoint}\" \
             -d '{\"source_blob\":{\"url\":\"#{@heroku_get_url}\"}}' \
             -H 'Accept: application/vnd.heroku+json; version=3' \
             -H \"Content-Type: application/json\""
-
-
-    # -d '{\"source_blob\":{\"url\":\"#{@heroku_get_url}\", \"version\": \"@branch_name\"}}' \
   end
 end
