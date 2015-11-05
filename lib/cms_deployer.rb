0df0a02af38bff4205fb8c4af007a397c4241645
johnlucia <johnmlucia@gmail.com>
2015-11-05 07:47:19 -0800

    some heroku debugging stuff

--- lib/cms_deployer.rb
+++ lib/cms_deployer.rb
@@ -15,11 +15,13 @@ class CmsDeployer
   end
 
   def self.deploy(source_url, app)
+    puts "\n\n\n\n*****\n  Start deploy: #{app} \n*****\n\n\n\n"
     heroku_build_endpoint = "https://api.heroku.com/apps/#{app}/builds"
     system "curl -n -X POST \"#{heroku_build_endpoint}\" \
             -d '{\"source_blob\":{\"url\":\"#{source_url}\"}}' \
             -H 'Accept: application/vnd.heroku+json; version=3' \
             -H \"Content-Type: application/json\""
+    puts "\n\n\n\n*****\n  End deploy \n*****\n\n\n\n"
   end
 
 
@@ -29,19 +31,25 @@ class CmsDeployer
   # Set up public GET and PUT urls on S3 to store the tar.gz we downloaded from Github
   # Note: Both these URL's expire in one hour.
   def generate_sources_endpoints
+    puts "\n\n\n\n*****\n  Start generate_sources_endpoints \n*****\n\n\n\n"
     response = HTTPClient.post @heroku_app_sources_endpoint, nil, HerokuApiHelpers.default_headers
     data = JSON.parse response.body
     @heroku_get_url = data["source_blob"]["get_url"]
     @heroku_put_url = data["source_blob"]["put_url"]
+    puts "\n\n\n\n*****\n  End generate_sources_endpoints \n*****\n\n\n\n"
   end
 
   # Download tar.gz from Github
   def download_from_gihub
+    puts "\n\n\n\n*****\n  Start download_from_gihub \n*****\n\n\n\n"
     system("curl -H 'Authorization: token #{ENV['GITHUB_TOKEN']}' -L #{@github_file_location} > #{@local_file_location}")
+    puts "\n\n\n\n*****\n  Start download_from_gihub \n*****\n\n\n\n"
   end
 
   # Upload tar.gz to S3
   def upload_to_heroku
+    puts "\n\n\n\n*****\n  Start upload_to_heroku \n*****\n\n\n\n"
     system("curl \"#{@heroku_put_url}\" -X PUT -H 'Content-Type:' --data-binary @#{@local_file_location}")
+    puts "\n\n\n\n*****\n  Start upload_to_heroku \n*****\n\n\n\n"
   end
 end
