9c36f56c36f1dcc49191439af7a91597a6031a12
johnlucia <johnmlucia@gmail.com>
2015-11-05 06:43:16 -0800

    try reusing the sources endpoint

--- lib/cms_tar_ball.rb
+++ lib/cms_tar_ball.rb
@@ -1,18 +1,21 @@
 class CmsTarBall
 
-  def initialize(app_name, branch_name = 'master')
+  def initialize(apps, branch_name = 'master')
     @branch_name = branch_name
     @github_file_location = "https://api.github.com/repos/g5search/g5-content-management-system/tarball/#{branch_name}"
     @local_file_location = "#{Rails.root.to_s}/tmp/#{@branch_name}-#{Time.now.to_i}.tar.gz"
-    @heroku_app_sources_endpoint = "https://api.heroku.com/apps/#{app_name}/sources"
-    @heroku_build_endpoint = "https://api.heroku.com/apps/#{app_name}/builds"
+    @heroku_app_sources_endpoint = "https://api.heroku.com/apps/#{apps.first}/sources"
     generate_sources_endpoints
+    download_from_gihub
+    upload_to_heroku
   end
 
-  def deploy_to_heroku
-    download
-    upload
-    deploy_cms
+  def deploy(cms_name)
+    heroku_build_endpoint = "https://api.heroku.com/apps/#{cms_name}/builds"
+    system "curl -n -X POST \"#{@heroku_build_endpoint}\" \
+            -d '{\"source_blob\":{\"url\":\"#{@heroku_get_url}\"}}' \
+            -H 'Accept: application/vnd.heroku+json; version=3' \
+            -H \"Content-Type: application/json\""
   end
 
   private
@@ -27,20 +30,12 @@ class CmsTarBall
   end
 
   # Download tar.gz from Github
-  def download
+  def download_from_gihub
     system("curl -H 'Authorization: token #{ENV['GITHUB_TOKEN']}' -L #{@github_file_location} > #{@local_file_location}")
   end
 
   # Upload tar.gz to S3
-  def upload
+  def upload_to_heroku
     system("curl \"#{@heroku_put_url}\" -X PUT -H 'Content-Type:' --data-binary @#{@local_file_location}")
-  end
-
-  # Tell Heroku to deploy the CMS from the tar.gz stored at @heroku_get_url
-  def deploy_cms
-    system "curl -n -X POST \"#{@heroku_build_endpoint}\" \
-            -d '{\"source_blob\":{\"url\":\"#{@heroku_get_url}\"}}' \
-            -H 'Accept: application/vnd.heroku+json; version=3' \
-            -H \"Content-Type: application/json\""
   end
 end
