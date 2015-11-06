5203c5387df73c2bda384aa0c44881bb18669459
Ryan Bales <ryankbales@gmail.com>
2015-11-05 19:44:18 -0800

    restructuring redis keys and changing specs to pass

--- app/workers/performance_dashboard_worker.rb
+++ app/workers/performance_dashboard_worker.rb
@@ -10,41 +10,44 @@ class PerformanceDashboardWorker
 
   def new_relic_data
     data = PerformanceData.get_new_relic_data("https://api.newrelic.com/v2/applications.json")
-    data["applications"].each do |app|
+    apps = data["applications"].map do |app|
       unless app["reporting"] == false
-        redis.set("new_relic_#{app["id"]}", 
-                  [app["name"],
-                   app["health_status"],
-                   app["application_summary"]["response_time"],
-                   app["application_summary"]["apdex_score"],
-                   app["application_summary"]["error_rate"]
-        ].to_json)
+        {
+          id: app["id"],
+          name: app["name"],
+          health_status: app["health_status"],
+          response_time: app["application_summary"]["response_time"],
+          apdex_score: app["application_summary"]["apdex_score"],
+          error_rate: app["application_summary"]["error_rate"]
+        }
       end
     end
+    redis.set("newrelic:data", apps.to_json)
   end
 
   def pagerduty_oncall
     data = PerformanceData.get_pagerduty_oncall("https://ey-g5search.pagerduty.com/api/v1/escalation_policies/on_call")
-    data["escalation_policies"][0]["on_call"].each do |goat|
-      redis.set("pager_duty_level_#{goat["level"]}",
-                [goat["user"]["name"],
-                 goat["level"],
-                 goat["start"],
-                 goat["end"]
-                ].to_json)
+    oncall = data["escalation_policies"][0]["on_call"].map do |goat|
+      {
+        user: goat["user"]["name"],
+        level: goat["level"],
+        start_date_time: goat["start"],
+        end_date_time: goat["end"]
+      }
     end
+    redis.set("pagerduty:oncall", oncall.to_json)
   end
 
   def pagerduty_incidents
     data = PerformanceData.get_pagerduty_incidents("https://ey-g5search.pagerduty.com/api/v1/incidents")
-    data["incidents"].each do |inci|
-      redis.set("pagerduty_incident_#{inci["incident_number"]}",
-                [
-                  inci["status"],
-                  inci["created_on"],
-                  inci["trigger_summary_data"]["description"]
-      ].to_json)
+    incidents = data["incidents"].map do |inci|
+      {
+        status: inci["status"],
+        created_at: inci["created_on"],
+        description: inci["trigger_summary_data"]["description"]
+      }
     end
+    redis.set("pagerduty:incidents", incidents.to_json)
   end
 
   private
--- spec/workers/performance_dashboard_worker_spec.rb
+++ spec/workers/performance_dashboard_worker_spec.rb
@@ -13,35 +13,35 @@ describe PerformanceDashboardWorker do describe :perform do
       it "sets the appropriate redis keys and values" do
         PerformanceDashboardWorker.perform_async
         parsed_response = JSON.parse new_relic_response.to_json
-        expect(JSON.parse(redis.get("new_relic_1"))[0]).to eq(parsed_response["applications"][0]["name"])
-        expect(JSON.parse(redis.get("new_relic_1"))[1]).to eq(parsed_response["applications"][0]["health_status"])
-        expect(JSON.parse(redis.get("new_relic_1"))[2]).to eq(parsed_response["applications"][0]["application_summary"]["response_time"])
-        expect(JSON.parse(redis.get("new_relic_1"))[3]).to eq(parsed_response["applications"][0]["application_summary"]["apdex_score"])
-        expect(JSON.parse(redis.get("new_relic_1"))[4]).to eq(parsed_response["applications"][0]["application_summary"]["error_rate"])
+        expect(JSON.parse(redis.get("newrelic:data"))[0]["name"]).to eq(parsed_response["applications"][0]["name"])
+        expect(JSON.parse(redis.get("newrelic:data"))[0]["health_status"]).to eq(parsed_response["applications"][0]["health_status"])
+        expect(JSON.parse(redis.get("newrelic:data"))[0]["response_time"]).to eq(parsed_response["applications"][0]["application_summary"]["response_time"])
+        expect(JSON.parse(redis.get("newrelic:data"))[0]["apdex_score"]).to eq(parsed_response["applications"][0]["application_summary"]["apdex_score"])
+        expect(JSON.parse(redis.get("newrelic:data"))[0]["error_rate"]).to eq(parsed_response["applications"][0]["application_summary"]["error_rate"])
       end
 
       it "does not set redis for an app that is not reporting" do
         PerformanceDashboardWorker.perform_async
-        redis.get("2").should be_nil
+        JSON.parse(redis.get("newrelic:data"))[1].should be_nil
       end
     end
     context "when getting PagerDuty on call data" do
       it "sets the appropriate redis keys and values" do
         PerformanceDashboardWorker.perform_async
         oncall_parsed_response = JSON.parse pagerduty_oncall_response.to_json
-        expect(JSON.parse(redis.get("pager_duty_level_1"))[0]).to eq(oncall_parsed_response["escalation_policies"][0]["on_call"].first["user"]["name"])
-        expect(JSON.parse(redis.get("pager_duty_level_1"))[1]).to eq(oncall_parsed_response["escalation_policies"][0]["on_call"].first["level"])
-        expect(JSON.parse(redis.get("pager_duty_level_1"))[2]).to eq(oncall_parsed_response["escalation_policies"][0]["on_call"].first["start"])
-        expect(JSON.parse(redis.get("pager_duty_level_1"))[3]).to eq(oncall_parsed_response["escalation_policies"][0]["on_call"].first["end"])
+        expect(JSON.parse(redis.get("pagerduty:oncall"))[0]["user"]).to eq(oncall_parsed_response["escalation_policies"][0]["on_call"].first["user"]["name"])
+        expect(JSON.parse(redis.get("pagerduty:oncall"))[0]["level"]).to eq(oncall_parsed_response["escalation_policies"][0]["on_call"].first["level"])
+        expect(JSON.parse(redis.get("pagerduty:oncall"))[0]["start_date_time"]).to eq(oncall_parsed_response["escalation_policies"][0]["on_call"].first["start"])
+        expect(JSON.parse(redis.get("pagerduty:oncall"))[0]["end_date_time"]).to eq(oncall_parsed_response["escalation_policies"][0]["on_call"].first["end"])
       end
     end
     context "when getting PagerDuty incident data" do
       it "sets the appropriate redis keys and values" do
         PerformanceDashboardWorker.perform_async
         incident_parsed_response = JSON.parse pagerduty_incidents_response.to_json
-        expect(JSON.parse(redis.get("pagerduty_incident_1"))[0]).to eq(incident_parsed_response["incidents"].first["status"])
-        expect(JSON.parse(redis.get("pagerduty_incident_1"))[1]).to eq(incident_parsed_response["incidents"].first["created_on"])
-        expect(JSON.parse(redis.get("pagerduty_incident_1"))[2]).to eq(incident_parsed_response["incidents"].first["trigger_summary_data"]["description"])
+        expect(JSON.parse(redis.get("pagerduty:incidents"))[0]["status"]).to eq(incident_parsed_response["incidents"].first["status"])
+        expect(JSON.parse(redis.get("pagerduty:incidents"))[0]["created_at"]).to eq(incident_parsed_response["incidents"].first["created_on"])
+        expect(JSON.parse(redis.get("pagerduty:incidents"))[0]["description"]).to eq(incident_parsed_response["incidents"].first["trigger_summary_data"]["description"])
       end
     end
   end
