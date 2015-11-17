e9166300258d68320e98e49c30ee38aa9bdc9839
Ryan Bales <ryankbales@gmail.com>
2015-11-17 11:46:25 -0800

    adding top five cost guzzling apps and some other minor tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,7 +47,7 @@ module ApplicationHelper
         app.dynos.each do |dyno|
           grand_total += dyno["quantity"] if dyno.class == Hash
         end
-      end 
+      end
     end
     average = grand_total.to_f/apps.length
     average.round(1)
@@ -70,5 +70,15 @@ module ApplicationHelper
     month_elapsed = latest_invoice_date.day / latest_invoice_date.end_of_month.day.to_f
     projected = (current_invoice.total / 100) / month_elapsed
     "$#{projected.round}"
+  end
+
+  def top_five_most_costly_apps(organizations)
+    totals_hash = {}
+    organizations.each do |org|
+      cost = projected_monthly_cost(org).gsub(/\$?/, "").to_i
+      totals_hash[org] = cost
+    end
+    desc_totals = totals_hash.sort_by {|k, v| v}.reverse
+    desc_totals.take(5)
   end
 end
