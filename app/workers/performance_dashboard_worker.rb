ce1bf39912cab3d1cec7c1be0fac5e1d3c16594f
Ryan Bales <ryankbales@gmail.com>
2015-11-24 12:38:44 -0800

    skinny up the worker

--- app/workers/performance_dashboard_worker.rb
+++ app/workers/performance_dashboard_worker.rb
@@ -3,52 +3,9 @@ class PerformanceDashboardWorker
   include WorkersHelper
 
   def perform
-    new_relic_data
-    pagerduty_oncall
-    pagerduty_incidents
-  end
-
-  def new_relic_data
-    data = PerformanceData.get_new_relic_data("https://api.newrelic.com/v2/applications.json")
-    apps = data["applications"].map do |app|
-      unless app["reporting"] == false
-        {
-          id: app["id"],
-          name: app["name"],
-          health_status: app["health_status"],
-          response_time: app["application_summary"]["response_time"],
-          apdex_score: app["application_summary"]["apdex_score"],
-          error_rate: app["application_summary"]["error_rate"]
-        }
-      end
-    end
-    $redis.set("newrelic:data", apps.to_json)
-  end
-
-  def pagerduty_oncall
-    data = PerformanceData.get_pagerduty_oncall("https://ey-g5search.pagerduty.com/api/v1/escalation_policies/on_call")
-    oncall = data["escalation_policies"][0]["on_call"].map do |goat|
-      {
-        user: goat["user"]["name"],
-        level: goat["level"],
-        start_date_time: goat["start"],
-        end_date_time: goat["end"]
-      }
-    end
-    $redis.set("pagerduty:oncall", oncall.to_json)
-  end
-
-  def pagerduty_incidents
-    data = PerformanceData.get_pagerduty_incidents("https://ey-g5search.pagerduty.com/api/v1/incidents")
-    incidents = data["incidents"].map do |inci|
-      {
-        incident_number: inci["incident_number"],
-        status: inci["status"],
-        created_at: inci["created_on"],
-        description: inci["trigger_summary_data"]["description"]
-      }
-    end
-    $redis.set("pagerduty:incidents", incidents.to_json)
+    PerformanceData.new_relic_data
+    PerformanceData.pagerduty_oncall
+    PerformanceData.pagerduty_incidents
   end
 
 end
--- lib/performance_data.rb
+++ lib/performance_data.rb
@@ -1,22 +1,65 @@
 class PerformanceData
+  def self.new_relic_data
+    data = self.get_new_relic_data("https://api.newrelic.com/v2/applications.json")
+    apps = data["applications"].map do |app|
+      unless app["reporting"] == false
+        {
+          id: app["id"],
+          name: app["name"],
+          health_status: app["health_status"],
+          response_time: app["application_summary"]["response_time"],
+          apdex_score: app["application_summary"]["apdex_score"],
+          error_rate: app["application_summary"]["error_rate"]
+        }
+      end
+    end
+    $redis.set("newrelic:data", apps.to_json)
+  end
+
+  def self.pagerduty_oncall
+    data = self.get_pagerduty_oncall("https://ey-g5search.pagerduty.com/api/v1/escalation_policies/on_call")
+    oncall = data["escalation_policies"][0]["on_call"].map do |goat|
+      {
+        user: goat["user"]["name"],
+        level: goat["level"],
+        start_date_time: goat["start"],
+        end_date_time: goat["end"]
+      }
+    end
+    $redis.set("pagerduty:oncall", oncall.to_json)
+  end
+
+  def self.pagerduty_incidents
+    data = self.get_pagerduty_incidents("https://ey-g5search.pagerduty.com/api/v1/incidents")
+    incidents = data["incidents"].map do |inci|
+      {
+        incident_number: inci["incident_number"],
+        status: inci["status"],
+        created_at: inci["created_on"],
+        description: inci["trigger_summary_data"]["description"]
+      }
+    end
+    $redis.set("pagerduty:incidents", incidents.to_json)
+  end
+
   def self.get_new_relic_data(uri)
     begin
       response = HTTPClient.
-        get(uri, nil, { "X-Api-Key" => ENV['NEW_RELIC_API_KEY'] }) 
+        get(uri, nil, { "X-Api-Key" => ENV['NEW_RELIC_API_KEY'] })
       JSON.parse response.body
     rescue => e
       e.response
     end
   end
-    
+
   def self.get_pagerduty_oncall(uri)
     begin
       response = HTTPClient.
         get(uri,
             nil,
-            { 
+            {
               "Content-type" => "application/json",
-              "Authorization" => "Token token=#{ENV['PAGER_DUTY_API_KEY']}" 
+              "Authorization" => "Token token=#{ENV['PAGER_DUTY_API_KEY']}"
             }
         )
         JSON.parse response.body
@@ -30,9 +73,9 @@ class PerformanceData
       response = HTTPClient.
         get(uri,
             {"status"=>"triggered,acknowledged"},
-            { 
+            {
               "Content-type" => "application/json",
-              "Authorization" => "Token token=#{ENV['PAGER_DUTY_API_KEY']}" 
+              "Authorization" => "Token token=#{ENV['PAGER_DUTY_API_KEY']}"
             }
         )
         JSON.parse response.body
