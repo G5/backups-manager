196cd05a9c15774f3f15ff6d926a4fc56b90b951
Ryan Bales <ryankbales@gmail.com>
2016-01-07 14:19:27 -0800

    remove no longer used methods

--- app/controllers/performance_dashboard_controller.rb
+++ app/controllers/performance_dashboard_controller.rb
@@ -6,14 +6,10 @@ class PerformanceDashboardController < ApplicationController
     @goat = get_redis_data("pagerduty:oncall")
     @incidents = get_redis_data("pagerduty:incidents", "No Incidents at this time")
 
-    #for real time incident hotness
-    @new_incidents = new_incidents(@incidents, params["after"]) if params["after"]
-
     @unhealthy_apps = get_g5ops_data("g5ops:health")
 
     respond_to do |format|
       format.html
-      format.js
       format.json
     end
   end
@@ -22,18 +18,5 @@ class PerformanceDashboardController < ApplicationController
   def create
     PerformanceData.create_pagerduty_incident(params)
     render nothing: true
-  end
-
-  private
-
-  def new_incidents(incidents, param)
-    newbs = []
-    incidents.delete(:error_status)
-    incidents.each do |i|
-      if i[1].is_a?(Hash) && i[1].has_key?("incident_number")
-        newbs << i if i[1]["incident_number"] > param.to_i
-      end
-    end
-    newbs
   end
 end
