d45d2ee948cf4099fd92fb941d2ee9ba4fe166ae
Ryan Bales <ryankbales@gmail.com>
2016-01-14 14:45:59 -0800

    abstracting out method responsibilities

--- app/controllers/performance_dashboard_controller.rb
+++ app/controllers/performance_dashboard_controller.rb
@@ -4,7 +4,7 @@ class PerformanceDashboardController < ApplicationController
   def index
     @new_relic_data = get_redis_data("newrelic:data")
     @goat = get_redis_data("pagerduty:oncall")
-    @incidents = get_redis_data("pagerduty:incidents", "No Incidents at this time")
+    @incidents = get_pagerduty_incidents("pagerduty:incidents")
 
     @unhealthy_apps = get_g5ops_data("g5ops:health")
 
@@ -22,21 +22,21 @@ class PerformanceDashboardController < ApplicationController
 
   private
 
-  def get_redis_data(key, status_message="Data is currently unavailable.")
-    if key == "pagerduty:incidents"
-      if pagerduty_redis_incidents.present?
-        incident_keys = pagerduty_redis_incidents.reverse
-        incidents = Hash[status_message: false]
-        incident_keys.each do |pd_key|
-          inci_hash = { "#{pd_key}" => JSON.parse($redis.get(pd_key)) }
-          incidents.merge!(inci_hash)
-        end
-        return incidents
-      else
-        return Hash[status_message: "YeeHaw! No Incidents!"]
+  def get_redis_data(key)
+    ($redis.get(key) && JSON.parse($redis.get(key)).present?) ? Hash[status_message: false, data: JSON.parse($redis.get(key))] : Hash[status_message: "Data is currently unavailable."]
+  end
+
+  def get_pagerduty_incidents(key)
+    if pagerduty_redis_incidents.present?
+      incident_keys = pagerduty_redis_incidents.reverse
+      incidents = Hash[status_message: false]
+      incident_keys.each do |pd_key|
+        inci_hash = { "#{pd_key}" => JSON.parse($redis.get(pd_key)) }
+        incidents.merge!(inci_hash)
       end
+      return incidents
     else
-      ($redis.get(key) && JSON.parse($redis.get(key)).present?) ? Hash[status_message: false, data: JSON.parse($redis.get(key))] : Hash[status_message: status_message]
+      return Hash[status_message: "YeeHaw! No Incidents!"]
     end
   end
 
