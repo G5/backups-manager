1514728bae2853eb96aea9eb20cc8bdeccbe2f44
Randy Leighton <randyleighton@yahoo.com>
2016-01-28 13:52:17 -0800

    Remove development logging commands

--- app/workers/app_database_mover.rb
+++ app/workers/app_database_mover.rb
@@ -13,36 +13,14 @@ private
 
   def run_backups(app, heroku_bin_path, region, bucket_name)
     system_command = "#{heroku_bin_path} pg:backups public-url -a #{app.name}"
-
-    #Rails.logger.info("getting public-url") ##!
-
-    logger.info("running command: #{system_command}")
     public_url, stdeerr, status = Bundler.with_clean_env {Open3.capture3(system_command)}
-    logger.info("done")
-
-    logger.info("got public url value of: #{public_url}")
-    #public_url = Bundler.with_clean_env {system "#{system_command}"}
-    logger.info("app: #{app.name}")
     return unless status.success?
     public_url.strip!
-
-    #Rails.logger.info("had backups") ##!
-
-
     system_command = "curl -o #{Rails.root.join('tmp', app.name)} '#{public_url}'"
-    
-    #Rails.logger.info("downloading dump") ##!
-
     Bundler.with_clean_env {system "#{system_command}"}
     s3 = Aws::S3::Resource.new(region: region)
     bucket = s3.bucket(bucket_name)
-
-    #Rails.logger.info("uploading to s3") ##!
-
     upload_result = bucket.put_object({ body: File.open("#{Rails.root.join('tmp',app.name)}"), 
                                  key: "#{app.name}"})
-
-    #Rails.logger.info("result: #{upload_result.to_s}") ##!
   end
 end
-
