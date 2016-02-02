78a7d5793f5c8bace9b708720cfa90af088c2f52
Randy Leighton <randyleighton@yahoo.com>
2016-02-01 17:49:07 -0800

    update to set ENV to class constant and quick view success flag

--- app/workers/app_database_mover.rb
+++ app/workers/app_database_mover.rb
@@ -1,12 +1,14 @@
 require 'open3'
 
 class AppDatabaseMover
-
+  HEROKU_BIN_PATH, REGION, BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY =
+    ENV["HEROKU_BIN_PATH"], ENV["AWS_REGION"], ENV["S3_BUCKET_NAME"], ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"]
+  
   include Sidekiq::Worker
 
-  def perform(app_id, heroku_bin_path, region, bucket_name, aws_access_key_id, aws_secret_access_key)
-    app = App.find(app_id) 
-    run_backups(app, heroku_bin_path, region, bucket_name, aws_access_key_id, aws_secret_access_key)
+  def perform(app_id)#, heroku_bin_path, region, bucket_name, aws_access_key_id, aws_secret_access_key)
+    app = App.find(app_id)
+    run_backups(app, HEROKU_BIN_PATH, REGION, BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
   end
 
 private
@@ -24,8 +26,14 @@ private
     Bundler.with_clean_env {system "#{system_command}"}
     s3 = Aws::S3::Resource.new(region: region)
     bucket = s3.bucket(bucket_name)
-    upload_result = bucket.put_object({ body: File.open("#{Rails.root.join('tmp',app.name)}"), 
-                                 key: "#{app.name}"})
+    begin
+      bucket.put_object({ body: File.open("#{Rails.root.join('tmp',app.name)}"), key: "#{app.name}"})
+      app.backup_transfer_success = true 
+      logger.info("#{app.name} file saved to S3.")
+    rescue => e
+      app.backup_transfer_success = false 
+      logger.info("Failed to Save to S3. Error: #{e}")
+    end
+    app.save
   end
-
 end
