91b1d77d2996e56f419e65e7da93b3d11e6f8fea
Randy Leighton <randyleighton@yahoo.com>
2016-02-02 16:20:08 -0800

    method refactoring

--- app/workers/app_database_mover.rb
+++ app/workers/app_database_mover.rb
@@ -8,32 +8,51 @@ class AppDatabaseMover
 
   def perform(app_id)#, heroku_bin_path, region, bucket_name, aws_access_key_id, aws_secret_access_key)
     app = App.find(app_id)
-    run_backups(app, HEROKU_BIN_PATH, REGION, BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    run_backups(app)
   end
 
 private
 
-  def run_backups(app, heroku_bin_path, region, bucket_name, aws_access_key_id, aws_secret_access_key)
+  def run_backups(app)
     Aws.config.update({
-      region: region,
-      credentials: Aws::Credentials.new(aws_access_key_id, aws_secret_access_key)
+      region: REGION,
+      credentials: Aws::Credentials.new(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
     })
-    system_command = "#{heroku_bin_path} pg:backups public-url -a #{app.name}"
+    system_command = "#{HEROKU_BIN_PATH} pg:backups public-url -a #{app.name}"
     public_url, stdeerr, status = Bundler.with_clean_env {Open3.capture3(system_command)}
     return unless status.success?
     public_url.strip!
     system_command = "curl -o #{Rails.root.join('tmp', app.name)} '#{public_url}'"
     Bundler.with_clean_env {system "#{system_command}"}
-    s3 = Aws::S3::Resource.new(region: region)
-    bucket = s3.bucket(bucket_name)
-    begin
-      bucket.put_object({ body: File.open("#{Rails.root.join('tmp',app.name)}"), key: "#{app.name}"})
+    backup = File.open("#{Rails.root.join('tmp',app.name)}")   
+    if send_backup_to_s3(backup,app.name)
       app.backup_transfer_success = true 
-      logger.info("#{app.name} file saved to S3.")
-    rescue => e
+    else
       app.backup_transfer_success = false 
-      logger.info("Failed to Save to S3. Error: #{e}")
     end
     app.save
   end
+  
+  def send_backup_to_s3(backup, bucket_key)
+    s3 = Aws::S3::Resource.new(region: REGION)
+    bucket = s3.bucket(BUCKET_NAME)
+    begin
+      bucket.put_object({ body: backup, key: bucket_key})
+      logger.info("#{app.name} file saved to S3.")
+      true
+    rescue => e
+      logger.info("Failed to Save to S3. Error: #{e}")
+      false
+    end
+  end
+
+  def check_backup_schedule
+    schedule_check = "#{HEROKU_BIN_PATH} pg:backups public-url -a #{app.name}"
+
+  end
+
+  def get_public_url
+
+  end
+
 end
